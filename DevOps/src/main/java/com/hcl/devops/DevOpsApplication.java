d2d503dc9bdc0e60da1ad573b8cbab6f91f0f829
chetanaturankar89 <turankarc@gmail.com>
2021-11-20 13:45:23 +0530

    New Assignment

---
+++ DevOps/src/main/java/com/hcl/devops/DevOpsApplication.java
@@ -0,0 +1,15 @@
+package com.hcl.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsApplication.class, args);
+		
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
